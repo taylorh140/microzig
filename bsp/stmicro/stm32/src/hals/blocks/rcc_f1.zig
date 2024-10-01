1d26c098fe8264466268e915fb034a919f6ee056
Taylor <Taylor.Hillegeist@ultra-ice.com>
2024-10-01 15:45:34 -0500

    Checked Blinky Works.

--- bsp/stmicro/stm32/src/hals/blocks/rcc_f1.zig
+++ bsp/stmicro/stm32/src/hals/blocks/rcc_f1.zig
@@ -41,55 +41,6 @@ pub const rcc_f1 = extern struct {
         }
     }
 
-    pub fn enableLTDC(ref: @This()) void {
-        const self = ref.cell;
-        self.CR.PLLSAION = 0; //Disable PLL
 
-        for (0..50) |_| {
-            if (self.CR.PLLSAIRDY == 0) {
-                break;
-            }
-        }
-
-        self.PLLSAICFGR = .{
-            .PLLSAIN = 192,
-            .PLLSAIQ = self.PLLSAICFGR.PLLSAIQ,
-            .PLLSAIR = 4,
-        };
-
-        self.DCKCFGR.PLLSAIDIVR = .by8;
-
-        self.APB2ENR.LTDCEN = 1;
-        self.CR.PLLSAION = 1; //Enable PLL
-
-        for (0..50) |_| {
-            if (self.CR.PLLSAIRDY == 1) {
-                break;
-            }
-        }
-    }
-
-    pub fn enableDMA2D(ref: @This()) void {
-        const self = ref.cell;
-        self.AHB1ENR.DMA2DEN = 1;
-    }
 };
 
-// comptime {
-//     const A_t = packed struct(u32) { _Res1: u6 = 0, PLLSAIN: u9, _Res2: u9 = 0, PLLSAIQ: u4, PLLSAIR: u3, _Res3: u1 = 0 };
-//     var A: A_t = undefined;
-
-//     A = @bitCast(@as(u32, 0xFFFF_FFFF));
-//     @compileLog(.Error, A);
-//     A = .{
-//         .PLLSAIN = 0,
-//         .PLLSAIQ = A.PLLSAIQ,
-//         .PLLSAIR = 1,
-//     };
-//     @compileLog(.Error, A);
-
-//     // @compileLog(.Error, std.fmt.comptimePrint(",CSR = 0x{X}", .{@offsetOf(@This(), "CSR")}));
-//     // @compileLog(.Error, std.fmt.comptimePrint(",SSCGR = 0x{X}", .{@offsetOf(@This(), "SSCGR")}));
-//     // @compileLog(.Error, std.fmt.comptimePrint(",PLLI2SCFGR = 0x{X}", .{@offsetOf(RCC_CELL, "PLLI2SCFGR")}));
-//     std.debug.assert(@offsetOf(RCC_CELL, "PLLSAICFGR") == 0x88);
-// }
--- examples/stmicro/stm32/src/blinky.zig
+++ examples/stmicro/stm32/src/blinky.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 const microzig = @import("microzig");
 const stm32 = @import("microzig").hal;
 
-
 const led = stm32.GpioPin{.name = "LED1", .port=&stm32.GPIOC,.pin=13};
 
 pub fn main() !void {
+    stm32.RCC.enableGPIOport(.GPIOC);
     led.set_as_output(.low);
 
     while (true) {
